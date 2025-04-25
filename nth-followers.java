88f003d63034850dfb7c93a6050a938768248076
madhan45-mad <madhanmad476@gmail.com>
2025-04-25 17:51:07 +0530

    nth-followers.java

---
+++ nth-followers.java
@@ -0,0 +1,11 @@
+package com.example.nthfollowers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MutualFollowersApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MutualFollowersApplication.class, args);
+    }
+}
